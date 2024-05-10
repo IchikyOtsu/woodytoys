ccfcc95086cd5d19c2ba5c9815ba58e34965e56d
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-05-10 11:58:51 +0200

    Update main.py

--- services/api/main.py
+++ services/api/main.py
@@ -11,17 +11,14 @@ cors = CORS(app)
 
 # redis_db = redis.Redis(host='redis', port=6379, db=0)
 
-
 @app.get('/api/ping')
 def ping():
     return 'ping'
-
 
 # ### 1. Misc service ### (note: la traduction de miscellaneous est 'divers'
 @app.route('/api/misc/time', methods=['GET'])
 def get_time():
     return f'misc: {datetime.now()}'
-
 
 @app.route('/api/misc/heavy', methods=['GET'])
 def get_heavy():
@@ -31,7 +28,6 @@ def get_heavy():
     # on rajoute la date pour pas que le resultat ne soit mis en cache par le browser
     return f'{datetime.now()}: {r}'
 
-
 # ### 2. Product Service ###
 @app.route('/api/products', methods=['GET'])
 def add_product():
@@ -40,18 +36,15 @@ def add_product():
     woody.add_product(str(product))
     return str(product) or "none"
 
-
 @app.route('/api/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
     return "not yet implemented"
 
-
 @app.route('/api/products/last', methods=['GET'])
 def get_last_product():
     # TODO TP9: put in cache ? cache duration ?
-    last_product = woody.get_last_product()  # note: it's a very slow db query
+    last_product = woody.get_last_product() # note: it's a very slow db query
     return f'db: {datetime.now()} - {last_product}'
-
 
 # ### 3. Order Service
 @app.route('/api/orders/do', methods=['GET'])
@@ -60,29 +53,22 @@ def create_order():
     # order = request.get_json()
     product = request.args.get('product')
     order_id = str(uuid.uuid4())
-
     # TODO TP10: this next line is long, intensive and can be done asynchronously ... maybe use a message broker ?
     process_order(order_id, product)
-
     return f"Your process {order_id} has been created"
-
 
 @app.route('/api/orders/', methods=['GET'])
 def get_order():
     order_id = request.args.get('order_id')
     status = woody.get_order(order_id)
-
     return f'order "{order_id}": {status}'
-
 
 # #### 4. internal Services
 def process_order(order_id, order):
     # ...
     # ... do many check and stuff
     status = woody.make_heavy_validation(order)
-
     woody.save_order(order_id, status, order)
-
 
 if __name__ == "__main__":
     woody.launch_server(app, host='0.0.0.0', port=5000)
