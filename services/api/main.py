46cff1e8f44e4e53a39c5aa0ce5d9b8806ab5d2c
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-05-10 12:00:38 +0200

    Update main.py

--- services/api/main.py
+++ services/api/main.py
@@ -1,21 +1,22 @@
 import uuid
+import json
 from datetime import datetime
-
 from flask import Flask, request
 from flask_cors import CORS
-
+import redis
 import woody
 
 app = Flask('my_api')
 cors = CORS(app)
 
-# redis_db = redis.Redis(host='redis', port=6379, db=0)
+# Connexion à Redis
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 @app.get('/api/ping')
 def ping():
     return 'ping'
 
-# ### 1. Misc service ### (note: la traduction de miscellaneous est 'divers'
+# ### 1. Misc service ###
 @app.route('/api/misc/time', methods=['GET'])
 def get_time():
     return f'misc: {datetime.now()}'
@@ -25,13 +26,11 @@ def get_heavy():
     # TODO TP9: cache ?
     name = request.args.get('name')
     r = woody.make_some_heavy_computation(name)
-    # on rajoute la date pour pas que le resultat ne soit mis en cache par le browser
     return f'{datetime.now()}: {r}'
 
 # ### 2. Product Service ###
 @app.route('/api/products', methods=['GET'])
 def add_product():
-    # product = request.json.get('product', '')
     product = request.args.get('product')
     woody.add_product(str(product))
     return str(product) or "none"
@@ -42,18 +41,24 @@ def get_product(product_id):
 
 @app.route('/api/products/last', methods=['GET'])
 def get_last_product():
-    # TODO TP9: put in cache ? cache duration ?
-    last_product = woody.get_last_product() # note: it's a very slow db query
+    # Essayer de récupérer les données depuis le cache
+    cached_data = redis_client.get('last_product_cache')
+    if cached_data:
+        return cached_data
+
+    # Si pas en cache, récupérer depuis la DB
+    last_product = woody.get_last_product()  # note: it's a very slow db query
+
+    # Mettre en cache les données pour 60 secondes
+    redis_client.setex('last_product_cache', 60, json.dumps(f'db: {datetime.now()} - {last_product}'))
+
     return f'db: {datetime.now()} - {last_product}'
 
 # ### 3. Order Service
 @app.route('/api/orders/do', methods=['GET'])
 def create_order():
-    # very slow process because some payment validation is slow (maybe make it asynchronous ?)
-    # order = request.get_json()
     product = request.args.get('product')
     order_id = str(uuid.uuid4())
-    # TODO TP10: this next line is long, intensive and can be done asynchronously ... maybe use a message broker ?
     process_order(order_id, product)
     return f"Your process {order_id} has been created"
 
@@ -65,8 +70,6 @@ def get_order():
 
 # #### 4. internal Services
 def process_order(order_id, order):
-    # ...
-    # ... do many check and stuff
     status = woody.make_heavy_validation(order)
     woody.save_order(order_id, status, order)
 
