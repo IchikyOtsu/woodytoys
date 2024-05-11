be6dd88f3e69b368e8eb2f2e8c725f3132f0b57e
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-05-11 15:09:32 +0200

    Update build_push.sh

--- services/build_push.sh
+++ services/build_push.sh
@@ -40,4 +40,7 @@ docker push ichiky/woody_front:"$version"
 docker push ichiky/woody_front:latest
 
 docker push ichiky/woody_slave_database:"$version"
+docker push ichiky/woody_slave_database:latest
+
+docker push ichiky/woody_master_database:"$version"
 docker push ichiky/woody_master_database:latest
