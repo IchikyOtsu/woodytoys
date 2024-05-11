33801b88a37390a9d08fe62678427c26d06cc580
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-05-11 13:48:53 +0200

    Update build_push.sh

--- services/build_push.sh
+++ services/build_push.sh
@@ -9,6 +9,9 @@ version=${1:-"$default_version"}
 docker build -t ichiky/woody_api:"$version" api
 docker tag ichiky/woody_api:"$version" ichiky/woody_api:latest
 
+docker build -t ichiky/woody_apisec:"$version" api2
+docker tag ichiky/woody_apisec:"$version" ichiky/woody_apisec:latest
+
 docker build -t ichiky/woody_rp:"$version" reverse-proxy
 docker tag ichiky/woody_rp:"$version" ichiky/woody_rp:latest
 
@@ -22,6 +25,9 @@ docker tag ichiky/woody_front:"$version" ichiky/woody_front:latest
 
 docker push ichiky/woody_api:"$version"
 docker push ichiky/woody_api:latest
+
+docker push ichiky/woody_apisec:"$version"
+docker push ichiky/woody_apisec:latest
 
 docker push ichiky/woody_rp:"$version"
 docker push ichiky/woody_rp:latest
